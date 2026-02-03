de0374ff29d0bc2b0fba1723bc402305e4c7757e
Sanjana N H <sanjanahalepeti@gmail.com>
2026-02-03 21:46:13 +0530

    day2 done

---
+++ DAY2/python.py
@@ -0,0 +1,5 @@
+name= input("enter ur name :")
+age=input("enter ur current age:")
+age=int(age)
+new_age=age+5
+print("hello",name,"age after5 years be",new_age)
---
+++ DAY2/task2.py
@@ -0,0 +1,4 @@
+total_bill=float(input("enter the bill"))
+num_people=int(input("enter no of people"))
+bill_per_person=total_bill/num_people
+print(total_bill,"bill to be pay by each personis",bill_per_person)
